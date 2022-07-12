3c4b7da06672bb37a45aa0bb915784bbf57bdc94
0xStarkWheel <tarenadleyimj35@gmail.com>
2022-07-13 04:51:35 +0900

    feat: deploy-account script

---
+++ scripts/deploy-account.ts
@@ -0,0 +1,41 @@
+import {execSync} from 'child_process';
+import {readFileSync} from 'fs';
+import {homedir} from 'os';
+
+const NETWORK = 'alpha-goerli';
+interface AccountInfo {
+    PK: string;
+    pubKey: string;
+    address: string;
+}
+async function deployAccount(accountName: string) {
+    try {
+        const res = execSync(`starknet deploy_account --account=${accountName}`);        
+    } catch (error) {
+        console.log(error)
+    }
+    const accounts = readFileSync(homedir() + '/.starknet_accounts/starknet_open_zeppelin_accounts.json', 'utf8');
+    const accountInfo = JSON.parse(accounts)[NETWORK][accountName];
+    return <AccountInfo>{
+        PK: accountInfo.private_key,
+        pubKey: accountInfo.public_key,
+        address: accountInfo.address,
+    };
+}
+
+async function main() {
+    const accountName = 'newAccount3';
+    try {
+        const accountInfo = await deployAccount(accountName);
+        console.log(accountInfo);
+    } catch (error) {
+        console.log(error)        
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
