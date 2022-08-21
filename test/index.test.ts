30fdccad8f5f1ebe079370e18276001fac06d9a7
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-08-21 16:43:54 +0200

    feat: use latest deployment

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,9 +3,18 @@ import { Account, defaultProvider, ec } from 'starknet';
 import { Wallet } from '@ethersproject/wallet';
 
 describe('', () => {
+  // Factory: 0x3e5165026e6586cd2d5cdf1fdced8af866f900e430bf1dc7b839c84604c506e
+  // Spaces
+  // [vanilla auth, vanilla voting strategy]
+  // 0x03ddbcdfec756eb9e9e0fbce7a90721570a09fc97d31a15b8e40769b93ecb957
+  // [ethSig auth, vanilla voting strategy]
+  // 0x03a48d8a7e80b7b295ef15efc4e1604e78b1f4460266cd460b6eb8826846bba0
+  // [ethSig auth, single slot proof (WETH goerli)]
+  // 0x069555971fbf76b3d0471297818ed93986fdd7afe3816d53ea8d8e72034260d8
+
   const wallet = Wallet.createRandom();
   const walletAddress = wallet.address;
-  const space = '0x5ad4bd1ca422953ac845ac7915ddfa93fe3fecec0ed8c86db5e294b0e18c9bd';
+  const space = '0x03ddbcdfec756eb9e9e0fbce7a90721570a09fc97d31a15b8e40769b93ecb957';
   const client = new StarkNetTx();
   const address = '0x00c26a3cdcc570da83f3dd6afd0db9d038ee096e2c56707d6348db3b06223427';
   const privKey = '0x78a61bf2d838b1094705168bbd5b462e665a5ce094d8b1c2e5438c66eeb9f59';
@@ -26,7 +35,7 @@ describe('', () => {
 
   it('StarkNetTx.vote()', async () => {
     const voter = walletAddress;
-    const proposal = '3';
+    const proposal = '1';
     const choice = '1';
 
     const receipt = await client.vote(account, voter, space, proposal, choice);
