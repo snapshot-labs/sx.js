7729e9433bc6147bf31fbed8b526b496fb6373b5
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-09-09 00:08:05 +0200

    chore: remove old authenticators from constants

--- src/clients/starknet-tx/constants.ts
+++ src/clients/starknet-tx/constants.ts
@@ -1,8 +1,4 @@
 export default {
-  authenticators: {
-    '0x6ad07205a4d725c5c2b10c4f5fbdfaaa351c742fce7a5a22b2b56fd8d5afd62': 'vanilla',
-    '0x594a81b66c3aa2c64577916f727e1307b60c9d6afa80b6f5ca3e3049c40f643': 'ethSig'
-  },
   strategies: {
     '0x344a63d1f5cd0e5f707fede9886d5dd306e86eba91ea410b416f39e44c3865': 'vanilla',
     '0x4bbd8081b1e9ef84ee2a767ef2cdcdea0dd8298b8e2858afa06bed1898533e6': 'singleSlotProof'
