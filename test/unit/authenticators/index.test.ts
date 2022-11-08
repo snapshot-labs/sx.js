2f8b0ea6059919af6c41254c698d913d861cd522
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-11-08 20:47:10 +0000

    added default strategies and authenticators

--- test/unit/authenticators/index.test.ts
+++ test/unit/authenticators/index.test.ts
@@ -1,14 +1,36 @@
 import { getAuthenticator } from '../../../src/authenticators';
+import vanillaAuthenticator from '../../../src/authenticators/vanilla';
+import ethSigAuthenticator from '../../../src/authenticators/ethSig';
 
 describe('authenticators', () => {
   describe('getAuthenticator', () => {
-    it('should return correct authenticator with predefined addresses', () => {
+    it('should return correct authenticator from predefined default addresses', () => {
       expect(
         getAuthenticator('0xb32364e042cb948be62a09355595a4b80dfff4eb11a485c1950ace70b0e835')?.type
       ).toBe('vanilla');
 
       expect(
         getAuthenticator('0x6aac1e90da5df37bd59ac52b638a22de15231cbb78353b121df987873d0f369')?.type
+      ).toBe('ethSig');
+    });
+
+    it('should return correct authenticator from supplied addresses', () => {
+      const authenticators = {
+        '0x91469387a06fb88dd66559f4bc91e4c0c1e61e65c259b14a4d43f274fdabd922': vanillaAuthenticator,
+        '0x0091469387a06fb88dd66559f4bc91e4c0c1e61e65c259b14a4d43f274fdabd9': ethSigAuthenticator
+      };
+      expect(
+        getAuthenticator(
+          '0x91469387a06fb88dd66559f4bc91e4c0c1e61e65c259b14a4d43f274fdabd922',
+          authenticators
+        )?.type
+      ).toBe('vanilla');
+
+      expect(
+        getAuthenticator(
+          '0x0091469387a06fb88dd66559f4bc91e4c0c1e61e65c259b14a4d43f274fdabd9',
+          authenticators
+        )?.type
       ).toBe('ethSig');
     });
 
