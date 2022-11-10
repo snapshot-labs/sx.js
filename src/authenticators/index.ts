74d45d74faf42e1ba0f7ccfa43a27470dea7e697
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-11-10 12:24:31 +0000

    formatting

--- src/authenticators/index.ts
+++ src/authenticators/index.ts
@@ -6,7 +6,7 @@ import type { Authenticator } from '../types';
 const defaultAuthenticators = {
   '0xb32364e042cb948be62a09355595a4b80dfff4eb11a485c1950ace70b0e83500': vanillaAuthenticator,
   '0x06aac1e90da5df37bd59ac52b638a22de15231cbb78353b121df987873d0f369': ethSigAuthenticator
-}
+};
 
 export function getAuthenticator(
   address: string,
