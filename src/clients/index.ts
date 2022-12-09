e7f5edc6e6359a4e79917014705f7fde708992e2
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-12-09 23:19:53 +0100

    feat: add SpaceManager client

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,3 +1,4 @@
 export { EthereumSig } from './ethereum-sig';
 export { StarkNetSig } from './starknet-sig';
 export { StarkNetTx } from './starknet-tx';
+export { SpaceManager } from './space-manager';
