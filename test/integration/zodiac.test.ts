d5a514f079ea6ee3710215a0158914424b5b4c08
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-01-13 10:50:38 +0100

    refactor: accept Signer instead of Wallet (#119)

--- test/integration/zodiac.test.ts
+++ test/integration/zodiac.test.ts
@@ -7,7 +7,7 @@ describe('Zodiac', () => {
   expect(process.env.ETH_PK).toBeDefined();
 
   const provider = new JsonRpcProvider(process.env.GOERLI_NODE_URL);
-  const wallet = new Wallet(process.env.ETH_PK as string, provider);
+  const signer = new Wallet(process.env.ETH_PK as string, provider);
   const space = '0x07e6e9047eb910f84f7e3b86cea7b1d7779c109c970a39b54379c1f4fa395b28';
   const executor = '0x21dda40770f4317582251cffd5a0202d6b223dc167e5c8db25dc887d11eba81';
   const transactions = [
@@ -20,7 +20,7 @@ describe('Zodiac', () => {
     }
   ];
 
-  const zodiac = new Zodiac({ wallet });
+  const zodiac = new Zodiac({ signer });
 
   it('should receiveProposal', async () => {
     const receipt = await zodiac.receiveProposal(space, executor, {
