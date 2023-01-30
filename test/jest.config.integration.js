d167ac6f887a5c5202f3935444f97b3668498136
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-01-30 11:57:37 +0100

    test: pass files to test in args

--- test/jest.config.integration.js
+++ test/jest.config.integration.js
@@ -2,6 +2,5 @@ const baseConfig = require('./jest.config.base');
 
 module.exports = {
   ...baseConfig,
-  // TODO: EVM uses local node currently
-  roots: ['<rootDir>/integration/starknet']
+  roots: ['<rootDir>/integration']
 };
