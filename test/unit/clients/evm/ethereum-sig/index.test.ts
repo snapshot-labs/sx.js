2a246f45c6cad3374b478ced797808942208a120
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-03-22 18:22:52 +0100

    test: update tests

--- test/unit/clients/evm/ethereum-sig/index.test.ts
+++ test/unit/clients/evm/ethereum-sig/index.test.ts
@@ -4,8 +4,8 @@ import { EthereumSig } from '../../../../../src/clients/evm/ethereum-sig';
 import { Choice } from '../../../../../src/utils/choice';
 
 describe('EthereumSig', () => {
-  const space = '0x6d5608420702714a6c8168ec9c70706e8d1cf641';
-  const authenticator = '0x277f388b77cd36fff1c0e976c49a7c54413a449a';
+  const space = '0xef8cd9081d7969c5cdcbb84dd8d577d2daefb649';
+  const authenticator = '0xc537d997ddc783e071f82ccbfaa0d768d310001b';
   const executor = '0x6241b5c89350bb3c465179706cf26050ea32444f';
 
   const provider = new JsonRpcProvider('http://127.0.0.1:8545');
@@ -29,7 +29,7 @@ describe('EthereumSig', () => {
       data: {
         space,
         authenticator,
-        strategies: [{ index: 0, address: '0xf3e55d22845689be3e062975444d09799e522a6c' }],
+        strategies: [{ index: 0, address: '0xeba53160c146cbf77a150e9a218d4c2de5db6b51' }],
         executionStrategy: { addy: executor, params: '0x00' },
         metadataUri: 'ipfs://QmNrm6xKuib1THtWkiN5CKtBEerQCDpUtmgDqiaU2xDmca'
       }
@@ -44,7 +44,7 @@ describe('EthereumSig', () => {
       data: {
         space,
         authenticator,
-        strategies: [{ index: 0, address: '0xf3e55d22845689be3e062975444d09799e522a6c' }],
+        strategies: [{ index: 0, address: '0xeba53160c146cbf77a150e9a218d4c2de5db6b51' }],
         proposal: 1,
         choice: Choice.FOR,
         metadataUri: ''
