9ce4345e6aa9c4316e509c9f91c8f589e014768a
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-08-23 14:54:09 +0200

    feat: add L1 executor

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -2,5 +2,6 @@ export { StarkNetTx } from './starknet/starknet-tx';
 export { EthereumTx } from './starknet/ethereum-tx';
 export { EthereumSig } from './starknet/ethereum-sig';
 export { StarkNetSig } from './starknet/starknet-sig';
+export { L1Executor } from './starknet/l1-executor';
 export { EthereumTx as EvmEthereumTx } from './evm/ethereum-tx';
 export { EthereumSig as EvmEthereumSig } from './evm/ethereum-sig';
---
+++ src/clients/starknet/l1-executor/index.ts
@@ -0,0 +1,87 @@
+import { Signer } from '@ethersproject/abstract-signer';
+import { Contract, ContractFactory } from '@ethersproject/contracts';
+import L1ExecutorContract from './abis/L1AvatarExecutionStrategy.json';
+
+type DeployParams = {
+  owner: string;
+  target: string;
+  starknetCore: string;
+  executionRelayer: string;
+  starknetSpaces: string[];
+};
+
+type ExecuteParams = {
+  executor: string;
+  space: string;
+  proposal: {
+    startTimestamp: bigint;
+    minEndTimestamp: bigint;
+    maxEndTimestamp: bigint;
+    finalizationStatus: 0 | 1 | 2;
+    executionPayloadHash: string;
+    executionStrategy: string;
+    authorAddressType: 0 | 1 | 2;
+    author: string;
+    activeVotingStrategies: string;
+  };
+  votesFor: string;
+  votesAgainst: string;
+  votesAbstain: string;
+  executionHash: string;
+  payload: string;
+};
+
+export class L1Executor {
+  async deploy({
+    signer,
+    params: { owner, target, starknetCore, executionRelayer, starknetSpaces }
+  }: {
+    signer: Signer;
+    params: DeployParams;
+  }) {
+    const factory = new ContractFactory(
+      L1ExecutorContract.abi,
+      L1ExecutorContract.bytecode.object,
+      signer
+    );
+
+    const deploy = await factory.deploy(
+      owner,
+      target,
+      starknetCore,
+      executionRelayer,
+      starknetSpaces
+    );
+
+    return {
+      address: deploy.address,
+      txId: deploy.deployTransaction.hash
+    };
+  }
+
+  async execute({
+    signer,
+    executor,
+    space,
+    proposal,
+    votesFor,
+    votesAgainst,
+    votesAbstain,
+    executionHash,
+    payload
+  }: {
+    signer: Signer;
+  } & ExecuteParams) {
+    const contract = new Contract(executor, L1ExecutorContract.abi, signer);
+
+    return contract.execute(
+      space,
+      proposal,
+      votesFor,
+      votesAgainst,
+      votesAbstain,
+      executionHash,
+      payload
+    );
+  }
+}
