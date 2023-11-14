2a41ad8dda5a06d5d52c124ce25dd154b96eef24
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-11-14 17:25:17 +0100

    chore: update to latest eslint/prettier configs (#233)
    * test: update erc20votes tests to use different provider
    
    * chore: update to latest eslint/prettier configs

--- src/utils/encoding/starknet-execution-params.ts
+++ src/utils/encoding/starknet-execution-params.ts
@@ -1,5 +1,6 @@
 import { Call } from 'starknet';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function createStarknetExecutionParams(callArray: Call[]): string[] {
   return [];
 }
